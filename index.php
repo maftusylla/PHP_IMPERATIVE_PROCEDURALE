f85aaed9f708c89a7b0a80c067a86cfa1f8d378d
maftusylla <mamefatousylla003@gmail.com>
2026-07-01 01:21:09 +0000

    feat: initialisation des categories

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+//1-Initialisation des catégories
+$categories=[
+                0=>[
+                    "code" => "code0",
+                    "nom" => "alimentaire",
+                    "produits"=> 
+                        [  
+                            0=>[
+                                    "nom" => "yaourt",
+                                    "reference" => "ref1",
+                                    "prix" => 1200, 
+                                    "quantite" => 30
+                            ], 
+                            1=>[
+                                    "nom"=>"cafe",
+                                    "reference"=>"ref2",
+                                    "prix"=>600,
+                                    "quantite"=>100
+                                ],
+                        ],
+                    ],
+                1=>[
+                    "code" => "code1",
+                    "nom" => "cosmetique",
+                    "produits" => []
+                    ]
+
+               
+];
